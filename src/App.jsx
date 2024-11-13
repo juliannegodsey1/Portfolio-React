356ca064ca61d677fd8be83bf80468e2368aaba4
juliannegodsey1 <juliannegodseyy@gmail.com>
2024-11-13 11:32:55 -0500

    media queries added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import AboutMe from "./components/about-me/AboutMe";
